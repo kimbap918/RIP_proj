bdd30db7cf47956910d3dc044d66ade60b89776b
최준혁 <sopung@choejunhyeog-ui-MacBookPro.local>
2022-11-25 17:49:55 +0900

    first commit

---
+++ champions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ChampionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'champions'
---
+++ summoners/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SummonersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'summoners'
