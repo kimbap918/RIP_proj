53a935aecb544b7431714b3cd83bad457205445c
최준혁 <sopung@choejunhyeog-ui-MacBookPro.local>
2022-11-26 16:33:19 +0900

    feat/project: 어플리케이션 모델링: 최준혁

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
+class User(AbstractUser):
+    birthday = models.DateTimeField(default=timezone.now)
+    email = models.EmailField(max_length=100)
+    agree = models.BooleanField(null=False, default=False)
+    
