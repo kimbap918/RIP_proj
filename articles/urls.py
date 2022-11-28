1b36761a32ceb5dc5006b8a5f026ffead2dae6fc
skgud <skgud9511@gmail.com>
2022-11-28 12:16:34 +0900

    feat : articles detail,delete view와 url 테스트 template 코드 작성 : 김나형

--- articles/urls.py
+++ articles/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('',views.index, name="index"),
     path('create/', views.article_create, name="create"),
     path('update/<int:article_pk>', views.article_update, name="update"),
+    path('<int:article_pk>/',views.article_detail, name='detail'),
+    path('delete/<int:article_pk>/',views.article_delete, name='delete'),
     path("main/", views.main, name="main"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -54,3 +54,23 @@ def article_update(request,article_pk):
 def main(request):
     return render(request, "articles/main.html")
 
+# articles 삭제
+def article_detail(request, pk):
+    # 특정 글을 가져온다.
+    articles = get_object_or_404(articles, pk=pk)
+    articles_form = ArticleForm()
+
+    # template에 객체 전달
+    context = {
+        "articles": articles,
+        # 역참조 (articles에 포함된 articles data를 전부 불러온다.)
+        "articles": articles.articles_set.all(),
+        "articles_form": articles_form,
+    }
+    return render(request, "articles/detail.html", context)
+
+
+def article_delete(request, pk):
+    Article.objects.get(pk=pk).delete()
+
+    return redirect('articles:delete')
