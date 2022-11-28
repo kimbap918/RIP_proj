2f541e1ac6e614adfdaf9e9acc0af5c423ff0fe7
wnsghd14 <wnsghd14@gmail.com>
2022-11-28 11:42:57 +0900

    add/articles:create,update:최준홍

--- articles/models.py
+++ articles/models.py
@@ -10,13 +10,13 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = ProcessedImageField(
-        upload_to='images/', 
-        blank=True,
-        processors=[ResizeToFill(1200, 960)],
-        format='JPEG',
-        options={'quality': 80},
-    )
+    # image = ProcessedImageField(
+    #     upload_to='images/', 
+    #     blank=True,
+    #     processors=[ResizeToFill(1200, 960)],
+    #     format='JPEG',
+    #     options={'quality': 80},
+    # )
     like_user = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="like_post"
     )
--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "articles"
 
 urlpatterns = [
-
+    path('',views.index, name="index"),
+    path('create/', views.article_create, name="create"),
+    path('update/<int:article_pk>', views.article_update, name="update"),
 ]
