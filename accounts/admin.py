c8f3502a819b643d795c5d1c574a2881346fc607
최준혁 <sopung@choejunhyeog-ui-MacBookPro.local>
2022-11-28 11:46:44 +0900

    feat/accounts: 로그인 이메일/아이디로 가능하게끔 변경: 최준혁

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserCreationForm
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = User
+    list_display = ['email', 'username',]
+
+admin.site.register(User, CustomUserAdmin)
