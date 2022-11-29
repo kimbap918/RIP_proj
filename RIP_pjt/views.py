acf28b1be26de268ea481a39dbaf1c9d909253ad
wnsghd14 <wnsghd14@gmail.com>
2022-11-29 09:49:48 +0900

    chore/ 어제자 기능구현 확인 및 수정:최준홍

---
+++ RIP_pjt/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse,HttpResponseForbidden
+def main(request):
+    return render(request, "main.html")
