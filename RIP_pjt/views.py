3bd07aea94a11cc52a785b26da9a0d7653e1304d
HYE <jelly12paw@gmail.com>
2022-11-29 16:23:21 +0900

    feat: 디자인 초안

--- RIP_pjt/views.py
+++ RIP_pjt/views.py
@@ -3,3 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse,HttpResponseForbidden
 def main(request):
     return render(request, "main.html")
+
+def base(request):
+    return render(request, 'base.html')
