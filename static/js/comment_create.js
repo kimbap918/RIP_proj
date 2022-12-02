5b04f1bfece063a0bf9c209aeb2bae4cdb2436ca
최준혁 <sopung@choejunhyeog-ui-MacBookPro.local>
2022-12-02 16:12:45 +0900

    chore/articles: 댓글 비동기 완성: 최준혁

--- static/js/comment_create.js
+++ static/js/comment_create.js
@@ -1,5 +1,6 @@
 
 const commentForm = document.querySelector('#comment-form')
+
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value
     // 기존 코드와 비교해서 딱히 바뀐것 없음 
     commentForm.addEventListener('submit', function (event) {
@@ -10,7 +11,7 @@ const commentForm = document.querySelector('#comment-form')
             headers: { 'X-CSRFToken': csrftoken },
             data: new FormData(commentForm)
         }).then(response => {
-            console.log(response.data)
+            // console.log(response.data)
             const comments = document.querySelector("#comments")
             comments.textContent = ""
             // views 에서 리턴한 commentData, user값 가져옴
@@ -36,7 +37,7 @@ const commentForm = document.querySelector('#comment-form')
                   comments.insertAdjacentHTML('beforeend', `
                   <div class="div_comment">
                     <a class="keyboard-comment-user" href="#" style="text-decoration: none;">${commentData[i].profile_name}</a>
-                    <i class="like-btn-comment bi ${like}" data-article-id="${articlePK}" data-comment-id="${commentData[i].commentPK}"></i>
+                    <i class="like-btn-comment bi ${like}" data-article-id="${articlePK}" data-comment-id="${commentData[i].commentPK}" onclick="likeBtnComment(this)"></i>
                     <span class="like-count-comment">${commentData[i].count}</span>
                     <p>${commentData[i].content}</p>
                     <p class="comment-control-delete btn btn-outline-danger mb-2" onclick="comment_delete(this)" id="comment-delete-${commentData[i].commentPK}" data-postdel-id="${articlePK}" data-commentdel-id="${commentData[i].commentPK}">삭제</p>
@@ -47,7 +48,7 @@ const commentForm = document.querySelector('#comment-form')
                     comments.insertAdjacentHTML('beforeend', `
                     <div class="comment">
                       <a class="keyboard-comment-user" href="#" style="text-decoration: none;">${commentData[i].profile_name}</a>
-                      <i class="like-btn-comment bi ${like}" data-article-id="${articlePK}" data-comment-id="${commentData[i].commentPK}"></i>
+                      <i class="like-btn-comment bi ${like}" data-article-id="${articlePK}" data-comment-id="${commentData[i].commentPK}" onclick="likeBtnComment(this)"></i>
                       <span class="like-count-comment">${commentData[i].count}</span>
                       <p>${commentData[i].content}</p>
                       <p class="comment-control-delete btn btn-outline-danger mb-2" onclick="comment_delete(this)" id="comment-delete-${commentData[i].commentPK}" data-postdel-id="${articlePK}" data-commentdel-id="${commentData[i].commentPK}">삭제</p>
