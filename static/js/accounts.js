c290ebbae3ec7710b616465c04362cd59030d7ed
skgud <skgud9511@gmail.com>
2022-12-04 13:23:32 +0900

    fix: main 991px 이하 반응형

--- static/js/accounts.js
+++ static/js/accounts.js
@@ -1,4 +1,3 @@
-
 const multiStepForm = document.querySelector("[data-multi-step]")
 const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
 // 현재 위치에 active를 줘라 
@@ -45,3 +44,53 @@ function showCurrentStep() {
     step.classList.toggle("active", index === currentStep)
   })
 }
+
+// input값에 따른 버튼 비활성화
+const formWrap = document.querySelector('.form_wrap_line');
+const idForm = document.querySelector('#user_id');
+const pwForm = document.querySelector('#user_pw');
+const loginButton = document.querySelector('#btn_sign');
+
+idForm.addEventListener('keyup', activeEvent);
+pwForm.addEventListener('keyup', activeEvent);
+loginButton.addEventListener('click', errorEvent);
+
+function activeEvent() {
+  switch(!(idForm.value && pwForm.value)){
+    case true : loginButton.disabled = true; break;
+    case false : loginButton.disabled = false; break
+  }
+}
+// function activeEvent() {
+//   switch(!(ckForm.value)){
+//     case true : checkButton.disabled = true; break;
+//     case false : checkButton.disabled = false; break
+//   }
+// }
+
+function activeEvent() {
+  switch(!(idForm.value && pwForm.value && nickForm.value)){
+    case true : signupButton.disabled = true; break;
+    case false : signupButton.disabled = false; break
+  }
+}
+
+function errorEvent() { 
+    formWrap.classList.add('error');
+}
+
+// let input = document.querySelector(".input");
+// let button = document.querySelector(".button");
+// button.disabled = true;
+// input.addEventListener("change", stateHandle);
+// function stateHandle() {
+//   if (document.querySelector(".input").value === "") {
+//     button.disabled = true; 
+//   } else {
+//     button.disabled = false;
+//   }
+// }
+// 현재 날짜 설정
+// month => 달 Date => 일
+// document.getElementById('currnetMonth').value= new Date().toISOString().slice(0, 7);
+// document.getElementById('currentDate').value = new Date().toISOString().substring(0, 10);
