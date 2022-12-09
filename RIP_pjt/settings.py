0c82719116d76ff488a5839bf798cfa42b4ff0e1
skgud <skgud9511@gmail.com>
2022-12-09 17:36:59 +0900

    feat: AWS 배포 시작/김나형

--- RIP_pjt/settings.py
+++ RIP_pjt/settings.py
@@ -48,7 +48,7 @@ API_KEY = get_secret("API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1","localhost","ripggbeanstalk-env.eba-s3vhvfzm.ap-northeast-2.elasticbeanstalk.com"]
 
 # Application definition
 
@@ -117,12 +117,34 @@ WSGI_APPLICATION = "RIP_pjt.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+	
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "rip_gg_rds", # 코드 블럭 아래 이미지 참고하여 입력
+        "USER": "postgres",
+        # 데이터베이스 생성 시 작성한 패스워드
+        "HOST": "rip-gg-rds.ckmcvzkpzzbf.ap-northeast-2.rds.amazonaws.com", # 코드 블럭 아래 이미지 참고하여 입력
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
@@ -185,15 +207,25 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 #     AWS_REGION,
 # )
 
-# DEBUG = os.getenv("DEBUG") == "True"
 
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == "True"
 
 if DEBUG: 
     MEDIA_URL = "/media/"
     MEDIA_ROOT = BASE_DIR / "media"
 
-else:   
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DATABASE_NAME"), # .env 파일에 value 작성
+            "USER": "postgres",
+            "PASSWORD": os.getenv("DATABASE_PASSWORD"), # .env 파일에 value 작성
+            "HOST": os.getenv("DATABASE_HOST"), # .env 파일에 value 작성
+            "PORT": "5432",
+        }
+    }
+    
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
@@ -205,6 +237,7 @@ else:
         AWS_STORAGE_BUCKET_NAME,
         AWS_REGION,
     )
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
