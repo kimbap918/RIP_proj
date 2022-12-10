f4a75e4c98f797ea4c1dd473004400baad78df13
HYE <jelly12paw@gmail.com>
2022-12-10 20:15:05 +0900

    chore: summoners app 챔피언 별 공략 영상 추가 / 박혜진

--- accounts/views.py
+++ accounts/views.py
@@ -174,8 +174,10 @@ def password(request, user_pk):
 
 # 회원 탈퇴
 
-def pre_delete(request,user_pk):
+
+def pre_delete(request, user_pk):
     return render(request, "accounts/pre_delete.html")
+
 
 def delete(request):
     if request.user.is_authenticated:
