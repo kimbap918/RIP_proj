76734c2c336979246500e466d68c5d19206c5c15
wnsghd14 <wnsghd14@gmail.com>
2022-12-13 23:44:49 +0900

    docs/readme.md/refac/project/최준홍

--- accounts/views.py
+++ accounts/views.py
@@ -328,7 +328,7 @@ state_token = secrets.token_urlsafe(16)
 
 def kakao_request(request):
     kakao_api = "https://kauth.kakao.com/oauth/authorize?"
-    redirect_uri = redirect_uri = "http://ripggbean-env.eba-tprx3bfx.ap-northeast-2.elasticbeanstalk.com/accounts/login/kakao/callback"
+    redirect_uri = "http://ripggbean-env.eba-tprx3bfx.ap-northeast-2.elasticbeanstalk.com/accounts/login/kakao/callback"
     client_id = "39c09c3e2d2a0741405cf64373d6a60a"  # # rest_api_key
     return redirect(
         f"{kakao_api}&client_id={client_id}&redirect_uri={redirect_uri}&response_type=code"
